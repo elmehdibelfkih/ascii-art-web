58ca22f0f51eba338f6fd11fa414b5cf10ad0d1a
Your Name <you@example.com>
2025-02-05 17:54:18 +0100

    js fixed

--- requirements/project/static/counter.js
+++
@@ -1,7 +0,0 @@
-const textArea = document.getElementById('str-input');
-const inputCounter = document.getElementById('inputCounter');
-
-textArea.addEventListener('input',function() {
-    const counter = textArea.nodeValue.length;
-    inputCounter.textContent = `${counter} character${counter !==1 ? 's' : ''}`
-});
