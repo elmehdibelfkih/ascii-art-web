c7138311af50369ea3b8e2b95f3b9aa441f58f12
elmehdibelfkih <elmehdibelfkih@gmail.com>
2025-02-09 01:50:03 +0100

    enhancement

--- requirements/project/internal/config.go
+++ requirements/project/internal/config.go
@@ -3,9 +3,13 @@ package internal
 import "html/template"
 
 const (
+	CharacterHeight     = 8
+	MAXINPUTLENGTH      = 200
+	STANDARD_HASH       = "ac85e83127e49ec42487f272d9b9db8b"
+	SHADOW_HASH         = "a49d5fcb0d5c59b2e77674aa3ab8bbb1"
+	THINKERTOY_HASH     = "86d9947457f6a41a18cb98427e314ff8"
 	INDEX_TEMPLATE_PATH = "./templates/index.html"
 	BANNERS_PATH        = "./banners/"
-	MAXINPUTLENGTH      = 200
 )
 
 var Font = make(map[rune][]string, 95)
@@ -15,10 +19,3 @@ type Art struct {
 	Title string
 	Lines string
 }
-
-const (
-	CharacterHeight = 8
-	STANDARD_HASH   = "ac85e83127e49ec42487f272d9b9db8b"
-	SHADOW_HASH     = "a49d5fcb0d5c59b2e77674aa3ab8bbb1"
-	THINKERTOY_HASH = "86d9947457f6a41a18cb98427e314ff8"
-)
